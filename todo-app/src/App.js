4e97396ee36cbb62bbc63d8daedfc1261e75e48d
Richard <forbannade@gmail.com>
2023-06-06 12:02:22 +0200

    Test

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -7,7 +7,6 @@ function App() {
     const [todos, setTodos] = useState(
         JSON.parse(localStorage.getItem('todos')) || []
     );
-    const [page, setPage] = useState(0);
 
     useEffect(() => {
         if (!todos.length) {
@@ -18,17 +17,7 @@ function App() {
                 }
             );
         }
-    }, [todos]);
-
-    const next = () => {
-        setPage(page + 1);
-    };
-
-    const prev = () => {
-        if (page > 0) {
-            setPage(page - 1);
-        }
-    };
+    }, []);
 
     const handleCompleted = (todo) => {
         const updatedTodos = todos.map((t) =>
@@ -49,20 +38,8 @@ function App() {
     return (
         <div className="App">
             <h1 className="title">Todo List Example</h1>
-            <div className="buttons">
-                <button className="button" onClick={prev} disabled={page === 0}>
-                    Prev
-                </button>
-                <button
-                    className="button"
-                    onClick={next}
-                    disabled={page === todos.length - 1}
-                >
-                    Next
-                </button>
-            </div>
             <ul className="todo-list">
-                {todos[page]?.map((todo) => (
+                {todos.map((todo) => (
                     <li
                         className={`todo-item ${
                             todo.completed ? 'completed' : ''
@@ -71,10 +48,10 @@ function App() {
                     >
                         {todo.title}
                         <button onClick={() => handleCompleted(todo)}>
-                            Mark as completed
+                            Done
                         </button>
                         <button onClick={() => handleUncompleted(todo)}>
-                            Mark as uncompleted
+                            Undo
                         </button>
                     </li>
                 ))}
