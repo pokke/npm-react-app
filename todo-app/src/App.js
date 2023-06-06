f310386a5bb58cd546c51f0cf33c4c4c4a74a06e
Richard <forbannade@gmail.com>
2023-06-06 12:09:14 +0200

    Removed mock data

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,30 +1,24 @@
 import React, { useState, useEffect } from 'react';
 import { markAsCompleted, markAsUncompleted } from 'pokkes-npm-package-utils';
-import fetchData from 'pokkes-npm-package';
 import './App.css';
 
 function App() {
     const [todos, setTodos] = useState(
         JSON.parse(localStorage.getItem('todos')) || []
     );
+    const [newTodo, setNewTodo] = useState('');
+    const [page, setPage] = useState(0);
+    const todosPerPage = 10;
 
     useEffect(() => {
-        if (!todos.length) {
-            fetchData('https://jsonplaceholder.typicode.com/todos').then(
-                (chunks) => {
-                    setTodos(chunks);
-                    localStorage.setItem('todos', JSON.stringify(chunks));
-                }
-            );
-        }
-    }, []);
+        localStorage.setItem('todos', JSON.stringify(todos));
+    }, [todos]);
 
     const handleCompleted = (todo) => {
         const updatedTodos = todos.map((t) =>
             t.id === todo.id ? markAsCompleted(t) : t
         );
         setTodos(updatedTodos);
-        localStorage.setItem('todos', JSON.stringify(updatedTodos));
     };
 
     const handleUncompleted = (todo) => {
@@ -32,14 +26,52 @@ function App() {
             t.id === todo.id ? markAsUncompleted(t) : t
         );
         setTodos(updatedTodos);
-        localStorage.setItem('todos', JSON.stringify(updatedTodos));
     };
+
+    const addTodo = () => {
+        const todo = {
+            id: todos.length + 1,
+            title: newTodo,
+            completed: false,
+        };
+        setTodos([...todos, todo]);
+        setNewTodo('');
+    };
+
+    const prevPage = () => {
+        setPage(page - 1);
+    };
+
+    const nextPage = () => {
+        setPage(page + 1);
+    };
+
+    const todosToShow = todos.slice(
+        page * todosPerPage,
+        (page + 1) * todosPerPage
+    );
 
     return (
         <div className="App">
             <h1 className="title">Todo List Example</h1>
+            <input
+                type="text"
+                value={newTodo}
+                onChange={(e) => setNewTodo(e.target.value)}
+                placeholder="Add new todo"
+            />
+            <button onClick={addTodo}>Add</button>
+            <button onClick={prevPage} disabled={page === 0}>
+                Previous
+            </button>
+            <button
+                onClick={nextPage}
+                disabled={page >= Math.ceil(todos.length / todosPerPage) - 1}
+            >
+                Next
+            </button>
             <ul className="todo-list">
-                {todos.map((todo) => (
+                {todosToShow.map((todo) => (
                     <li
                         className={`todo-item ${
                             todo.completed ? 'completed' : ''
